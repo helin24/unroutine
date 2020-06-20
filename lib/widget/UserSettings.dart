12dffa38e9fe5f4c382e4a2ac689eb68ab1355d3
Helin Shiah <helin.shiah@gmail.com>
2020-06-20 10:06:08 -0700

    Add level selection

--- lib/widget/UserSettings.dart
+++ lib/widget/UserSettings.dart
@@ -7,19 +7,31 @@ class UserSettings extends StatefulWidget {
   _UserSettingsState createState() => _UserSettingsState();
 }
 
+const String CLOCKWISE_PREFERENCE = 'clockwise';
+const String LEVEL_PREFERENCE = 'level';
+// TODO: These should probably come from server eventually
+const List<String> levels = ['Pre-bronze', 'Bronze', 'Silver', 'Gold'];
+
 class _UserSettingsState extends State<UserSettings> {
   bool clockwise = false;
   SharedPreferences preferences;
-  static const String CLOCKWISE = 'clockwise';
+  String level = levels[0];
 
   @override
   initState() {
     super.initState();
     SharedPreferences.getInstance().then((newPreferences) {
       preferences = newPreferences;
-      if (preferences.containsKey(CLOCKWISE)) {
+
+      if (preferences.containsKey(CLOCKWISE_PREFERENCE)) {
         setState(() {
-          clockwise = preferences.getBool(CLOCKWISE);
+          clockwise = preferences.getBool(CLOCKWISE_PREFERENCE);
+        });
+      }
+
+      if (preferences.containsKey(LEVEL_PREFERENCE)) {
+        setState(() {
+          level = preferences.getString(LEVEL_PREFERENCE);
         });
       }
     });
@@ -46,6 +58,22 @@ class _UserSettingsState extends State<UserSettings> {
                 ),
               ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Level '),
+                DropdownButton<String>(
+                  value: level,
+                  onChanged: onLevelChanged,
+                  items: levels.map(
+                    (String level) => DropdownMenuItem<String>(
+                      value: level,
+                      child: Text(level),
+                    ),
+                  ).toList(),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -56,6 +84,13 @@ class _UserSettingsState extends State<UserSettings> {
     setState(() {
       clockwise = newValue;
     });
-    preferences.setBool(CLOCKWISE, newValue);
+    preferences.setBool(CLOCKWISE_PREFERENCE, newValue);
+  }
+
+  onLevelChanged(String newValue) {
+    setState(() {
+      level = newValue;
+    });
+    preferences.setString(LEVEL_PREFERENCE, newValue);
   }
 }
