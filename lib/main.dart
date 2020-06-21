71771394b5e7008c5b5f01ce15f5c2404b7f0624
Helin Shiah <helin.shiah@gmail.com>
2020-06-21 12:14:31 -0700

    Decide start screen depending on settings presence

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:unroutine/widget/user_settings.dart';
 import 'package:unroutine/widget/generate_sequence.dart';
 
@@ -6,7 +7,36 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool loading = true;
+  bool hasSettings;
+
+  @override
+  initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((preferences) {
+      setState(() {
+        loading = false;
+        hasSettings = preferences.getKeys().length > 0;
+      });
+    });
+  }
+
+  Widget _getHome() {
+    if (loading) {
+      return CircularProgressIndicator();
+    }
+
+    return hasSettings
+        ? GenerateSequence(title: 'Generate a sequence')
+        : UserSettings();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +44,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: UserSettings(),
+      home: _getHome(),
       routes: <String, WidgetBuilder>{
         '/generate': (BuildContext context) =>
             GenerateSequence(title: 'Generate a sequence'),
