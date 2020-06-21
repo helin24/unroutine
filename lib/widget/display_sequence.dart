96422ee87ef516e99d6f9739dd96f46b5d8b5a04
Helin Shiah <helin.shiah@gmail.com>
2020-06-21 12:04:10 -0700

    Add tab to manage sequence (#4)
    * Add manage display page
    
    * Add unsave button
    
    * Add rating stars
    
    * Add TODO for sending ratings to server

--- lib/widget/display_sequence.dart
+++ lib/widget/display_sequence.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:unroutine/model/sequence_model.dart';
-import 'package:http/http.dart' as http;
 import 'package:unroutine/database.dart';
 import 'package:unroutine/widget/audio_display.dart';
+import 'package:unroutine/widget/manage_display.dart';
 import 'package:unroutine/widget/text_display.dart';
 import 'package:unroutine/widget/saved.dart';
-import 'dart:async';
-import 'dart:convert';
 
 import 'package:unroutine/widget/visual_display.dart';
 
@@ -36,10 +34,17 @@ class _DisplaySequenceState extends State<DisplaySequence> {
         MaterialPageRoute<void>(builder: (BuildContext context) => Saved()));
   }
 
+  void _onUnsave() {
+    setState(() {
+      saved = false;
+    });
+    // TODO: Remove from database when database is restructured to allow deletes.
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 4,
       child: Scaffold(
         appBar: AppBar(title: Text(widget.title), actions: <Widget>[
           IconButton(
@@ -51,12 +56,14 @@ class _DisplaySequenceState extends State<DisplaySequence> {
           TextDisplay(sequence: sequence, saved: saved),
           VisualDisplay(sequence: sequence, saved: saved),
           AudioDisplay(sequence: sequence, saved: saved),
+          ManageDisplay(sequence: sequence, saved: saved, onUnsave: _onUnsave,),
         ]),
         bottomNavigationBar: TabBar(
           tabs: [
             Tab(icon: Icon(Icons.font_download)),
             Tab(icon: Icon(Icons.remove_red_eye)),
             Tab(icon: Icon(Icons.hearing)),
+            Tab(icon: Icon(Icons.settings)),
           ],
           labelColor: Theme.of(context).accentColor,
           unselectedLabelColor: Theme.of(context).disabledColor,
---
+++ lib/widget/manage_display.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:unroutine/model/sequence_model.dart';
+import 'package:flutter/material.dart';
+
+class ManageDisplay extends StatefulWidget {
+  ManageDisplay({this.sequence, this.saved, this.onUnsave});
+
+  final SequenceModel sequence;
+  final bool saved;
+  final Function() onUnsave;
+
+  @override
+  _ManageDisplayState createState() => _ManageDisplayState();
+}
+
+class _ManageDisplayState extends State<ManageDisplay> {
+  double rating = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = [];
+
+    children.add(RatingBar(
+      initialRating: rating,
+      direction: Axis.horizontal,
+      itemCount: 4,
+      ratingWidget: RatingWidget(
+        full: Icon(Icons.star),
+        half: Icon(Icons.star),
+        empty: Icon(Icons.star_border),
+      ),
+      onRatingUpdate: (rating) {
+        setState(() {
+          rating = rating;
+        });
+        // TODO: Add call to server.
+      },
+    ));
+
+    if (widget.saved) {
+      children.add(FlatButton(
+        onPressed: widget.onUnsave,
+        child: Text('Unsave'),
+      ));
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: children,
+    );
+  }
+}
+
+Text getTransition(Transition transition) {
+  return Text(
+    transition.move.name +
+        ' -> ' +
+        transition.exit.foot +
+        transition.exit.abbreviation,
+  );
+}
