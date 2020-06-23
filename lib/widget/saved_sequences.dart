9eea7019ce05fa9d383cdd146f43f7684d80bf89
Helin Shiah <helin.shiah@gmail.com>
2020-06-22 19:49:09 -0700

    Remove unused commit

--- lib/widget/saved_sequences.dart
+++ lib/widget/saved_sequences.dart
@@ -4,7 +4,6 @@ import 'package:unroutine/database.dart';
 import 'package:unroutine/model/sequence_model.dart';
 import 'package:unroutine/widget/display_sequence.dart';
 import 'package:unroutine/widget/popup_menu.dart';
-import 'package:unroutine/widget/text_display.dart';
 
 class SavedSequences extends StatelessWidget {
   @override
