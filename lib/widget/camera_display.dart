37d826cb4426665b0c45a7504a130abcb835eb61
Helin Shiah <helin.shiah@gmail.com>
2020-06-23 20:03:34 -0700

    Use camera plugin to show camera view

--- lib/widget/camera_display.dart
+++ lib/widget/camera_display.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 import 'package:unroutine/model/sequence_model.dart';
 
 class CameraDisplay extends StatefulWidget {
@@ -11,8 +12,46 @@ class CameraDisplay extends StatefulWidget {
 }
 
 class _CameraDisplayState extends State<CameraDisplay> {
+  CameraController controller;
+  bool waiting = true;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsFlutterBinding.ensureInitialized();
+    availableCameras().then((cameras) {
+      if (cameras.length == 0) {
+        return;
+      }
+      controller = CameraController(cameras.first, ResolutionPreset.medium);
+      controller.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {
+          waiting = false;
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('hello');
+    if (waiting) {
+      return CircularProgressIndicator();
+    }
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio:
+        controller.value.aspectRatio,
+        child: CameraPreview(controller));
   }
 }
