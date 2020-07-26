43ce93b936bde54dbda62dd2f60b8210c6a94e44
Helin Shiah <helin.shiah@gmail.com>
2020-07-26 16:34:54 -0700

    Style text display (#28)
    * Fix audio URL extraction from JSON
    
    * Add colors and change alignment

--- lib/widget/text_display.dart
+++ lib/widget/text_display.dart
@@ -10,29 +10,63 @@ class TextDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Text> children = [
-      Text(sequence.startEdge.foot + sequence.startEdge.abbreviation)
-    ] +
-        sequence.transitions.map(getTransition).toList();
+    List<Text> children =
+        sequence.transitions.map((t) => getTransition(t, context)).toList();
 
     if (saved) {
       children.add(Text(''));
       children.add(Text('Saved!'));
     }
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: children,
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                sequence.startEdge.foot + sequence.startEdge.abbreviation,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: children,
+          ),
+        ],
+      ),
     );
   }
 
-}
+  Text getTransition(Transition transition, BuildContext context) {
+    return Text(
+      transition.move.name +
+          ' -> ' +
+          transition.exit.foot +
+          transition.exit.abbreviation,
+      style: TextStyle(
+        color: getTransitionColor(transition),
+        fontSize: 28,
+      ),
+    );
+  }
 
-Text getTransition(Transition transition) {
-  return Text(
-    transition.move.name +
-        ' -> ' +
-        transition.exit.foot +
-        transition.exit.abbreviation,
-  );
+  Color getTransitionColor(Transition transition) {
+    String word = transition.exit.foot + transition.exit.abbreviation[0];
+
+    switch (word) {
+      case 'LF':
+        return Colors.lightGreen;
+      case 'LB':
+        return Colors.green;
+      case 'RF':
+        return Colors.orange;
+      case 'RB':
+        return Colors.deepOrange;
+      default:
+        return Colors.black;
+    }
+  }
 }
